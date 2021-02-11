0b191f2afb1461f62ed2d1401d9b2ef44eb5f9f0
Wolfram Diestel <diestel@steloj.de>
2021-02-11 18:04:40 +0100

    +sub_result, kampojn kun not null ni ne bezonas apriore plenigi per ''

--- initdb.d/z0_kromtabeloj.sql
+++ initdb.d/z0_kromtabeloj.sql
@@ -22,12 +22,13 @@ CREATE TABLE `submeto` (
   `sub_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `sub_state` ENUM('nova','trakt','arkiv','eraro') NOT NULL DEFAULT 'nova', 
                                 -- trakt' = traktata, 'arkiv' = akceptita/arkivita, 'eraro' = rifuzita pro eraro
-  `sub_email` VARCHAR(50) CHARACTER SET utf8 NOT NULL DEFAULT '',
-  `sub_cmd` VARCHAR(20) CHARACTER SET utf8 NOT NULL DEFAULT '', -- 'aldono' por nova dosiero, principe eblus ankau forigo!
-  `sub_desc` VARCHAR(255) CHARACTER SET utf8 NOT NULL DEFAULT '', -- la ŝanĝpriskribo
+  `sub_email` VARCHAR(50) CHARACTER SET utf8 NOT NULL,
+  `sub_cmd` VARCHAR(20) CHARACTER SET utf8 NOT NULL DEFAULT 'redakto', -- 'aldono' por nova dosiero, principe eblus ankau forigo!
+  `sub_desc` VARCHAR(255) CHARACTER SET utf8 NOT NULL, -- la ŝanĝpriskribo
   `sub_type` VARCHAR(20) CHARACTER SET utf8 NOT NULL DEFAULT 'xml', -- 'zip/xml' por kunpremita xml
-  `sub_fname` VARCHAR(50) CHARACTER SET utf8 NOT NULL DEFAULT '', -- ĉe 'aldono' la nomo de nova dosiero
+  `sub_fname` VARCHAR(50) CHARACTER SET utf8 NOT NULL, -- ĉe 'aldono' la nomo de nova dosiero
   `sub_content` MEDIUMBLOB NOT NULL,
+  `sub_result` BLOB,
   INDEX inx_sub_state(`sub_state`),
   INDEX inx_sub_email(`sub_email`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;  
