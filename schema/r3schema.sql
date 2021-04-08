a7a1b3d19a917654b30618f33fdd3663c8273b27
Wolfram Diestel <diestel@steloj.de>
2021-04-08 20:11:58 +0200

    iom da adaptoj, korektoj, fnt->mrk en r3-skemo

--- schema/r3schema.sql
+++ schema/r3schema.sql
@@ -2,6 +2,7 @@ USE `db314802x3159000`;
 
 -- kapvortoj kun referenco al drv@mrk:
 -- mrk: drv@mrk, var: tld@var
+-- ofc: la unua (se escepte estas pluraj) ofc de la kapvorto: *, 1-9, 1953...
 CREATE TABLE `r3kap` (
     `kap` VARCHAR(100) NOT NULL,
     `mrk` VARCHAR(100) NOT NULL,
@@ -17,7 +18,7 @@ CREATE TABLE `r3kap` (
 -- num: aŭtomate atribuita numeroj, aparte de snc/subsnc: 1, 2a, 2b...
 -- drv: drv@mrk por konekti al r3kap.mrk
 CREATE TABLE `r3mrk` (
-    `mrk` VARCHAR(100) NOT NULL,
+    `mrk` VARCHAR(100) NOT NULL PRIMARY KEY,
     `ele` VARCHAR(10) NOT NULL,
     `num` VARCHAR(10),
     `drv` VARCHAR(100),
@@ -25,15 +26,15 @@ CREATE TABLE `r3mrk` (
     KEY `ele` (`ele`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- referencoj, i.a. por la tezaŭro, 'fnt' kaj 'cel' estas atributoj @mrk 
+-- referencoj, i.a. por la tezaŭro, 'mrk' kaj 'cel' estas atributoj @mrk 
 -- tradukeblaj al kapvortoj kaj numeroj per la tabeloj r3mrk kaj r3kap
 -- tip: interne estas cifera kolumno kun '' = 0, 'vid' = 1 ... 'sin' = 10
 CREATE TABLE `r3ref` (
-    `fnt` VARCHAR(100) NOT NULL,
-    `tip` ENUM('vid','ekz','lst','prt','malprt','sub','super','hom','ant','sin') DEFAULT '',
+    `mrk` VARCHAR(100) NOT NULL,
+    `tip` ENUM('','vid','ekz','lst','prt','malprt','sub','super','hom','ant','sin') DEFAULT '',
     `cel` VARCHAR(100) NOT NULL,
     `lst` VARCHAR(100) NOT NULL,
-    KEY `fnt` (`fnt`),
+    KEY `mrk` (`mrk`),
     KEY `cel` (`cel`),
     KEY `tip` (`tip`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
@@ -45,7 +46,7 @@ CREATE TABLE `r3ref` (
 CREATE OR REPLACE VIEW `v3tezauro` AS
 SELECT r.mrk, r.tip, r.cel, k.kap, k.var, m.num
 FROM (
-    SELECT fnt AS `mrk`, tip, cel, lst 
+    SELECT mrk, tip, cel, lst 
     FROM `r3ref`
   UNION 
     SELECT cel AS `mrk`,
@@ -53,10 +54,10 @@ FROM (
                WHEN 'sub' THEN 'super' WHEN 'super' THEN 'sub'
                WHEN 'ekz' THEN 'super' WHEN 'dif' THEN 'sin'
       ELSE tip, 
-    fnt AS `cel` 
+    mrk AS `cel` 
     FROM `r3ref`
 ) AS r
-LEFT JOIN r3mrk m ON r.cel = m.mrk
+LEFT JOIN r3mrk m ON r.mrk = m.mrk
 LEFT JOIN r3kap k ON m.drv = k.mrk;
 
 -- tradukoj por la serĉo de ne-esperantaj vortoj
