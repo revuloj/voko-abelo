5bfe349664f52c2b23ca977d386887fc5e5a2f30
Wolfram Diestel <wolfram.diestel@t-systems.com>
2021-05-06 17:32:38 +0200

    adapto forigu_proc al r3

--- schema/forigu-proc.sql
+++ schema/forigu-proc.sql
@@ -11,34 +11,24 @@ DELIMITER //
 CREATE PROCEDURE forigu_art(IN dosiero VARCHAR(255))
 BEGIN
   
-  DELETE FROM var 
-  WHERE var_drv_id in (SELECT drv_id FROM drv WHERE drv_art_id in 
-      (SELECT art_id FROM art WHERE art_amrk = dosiero));
+  DELETE FROM r3trd 
+  WHERE mrk = dosiero OR mrk LIKE CONCAT(dosiero,'.%');
 
-  DELETE FROM trd 
-  WHERE trd_snc_id in (SELECT snc_id FROM snc WHERE snc_drv_id in 
-       (SELECT drv_id FROM drv WHERE drv_art_id in (SELECT art_id FROM art WHERE art_amrk = dosiero)));
-
-  DELETE FROM rim WHERE rim_art_id in (SELECT art_id FROM art WHERE art_amrk = dosiero);
-
-  DELETE FROM snc 
-  WHERE snc_drv_id in (SELECT drv_id FROM drv WHERE drv_art_id in 
-       (SELECT art_id FROM art WHERE art_amrk = dosiero));
-
-  DELETE FROM drv 
-  WHERE drv_art_id in (SELECT art_id FROM art WHERE art_amrk = dosiero);
-
-  DELETE FROM art WHERE art_amrk = dosiero;
-
-  DELETE FROM r3kap WHERE mrk = dosiero OR mrk LIKE concat(dosiero,'.%');
-
-  DELETE FROM r3mrk WHERE mrk LIKE concat(dosiero,'.%');
-
+  DELETE FROM r3ref
+  WHERE mrk = dosiero OR mrk LIKE CONCAT(dosiero,'.%');
   -- referencoj povas ankaŭ montri al la forigenda artikolo
-  -- se pli bone forigu ilin el la fontaj artikoloj sekvante la eraroraporton
+  -- sed eble pli bone forigu ilin el la fontaj artikoloj sekvante la eraroraporton
   -- tiam ili malaperos ankaŭ el r3ref!
-  DELETE FROM r3ref WHERE mrk LIKE concat(dosiero,'.%');
-  
+
+  DELETE FROM r3mrk
+  WHERE mrk = dosiero OR mrk LIKE CONCAT(dosiero,'.%');
+
+  DELETE FROM r3kap
+  WHERE mrk = dosiero OR mrk LIKE CONCAT(dosiero,'.%');
+
+  DELETE FROM r2_vikicelo
+  WHERE vik_celref = dosiero OR vik_celref LIKE CONCAT(dosiero,'.%');
+
 END//
 
 DELIMITER ;
